409f311f684a9f1efa9f376534c46a642f44d0ca
hiroshijp <hiroshijp0616@gmail.com>
2024-09-19 17:29:27 +0900

    [update] hcce-observerへのPOSTを追加

--- src/index.js
+++ src/index.js
@@ -25,3 +25,24 @@ console.info(`Spoke version: ${process.env.BUILD_VERSION}`);
 const api = new Api();
 
 ReactDOM.render(<App api={api} />, document.getElementById("app"));
+
+// post access info to hubs-observer
+var email = "anonymous";
+var from = window.location.href;
+const localStorageData = localStorage.getItem("___hubs_store");
+if (localStorageData) {
+    email = JSON.parse(localStorageData).credentials.email;
+} 
+
+const req = {
+ visitor: { mail: email },
+ visited_from: from,
+};
+
+fetch('https://hcce-observer-955595017457.us-central1.run.app/visited', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(req)
+})
